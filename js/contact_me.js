21c731211b6912bc130de5a0ae7156666a3740ec
Coral Silverman <coral.silverman@kaplan.com>
2017-05-28 12:27:26 -0400

    rewrite jQuery functions in vanilla javascript

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,70 +1,89 @@
+(function() {
+  
+  (function enableSend() {
+    var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var message = document.getElementById('message');
+    name.addEventListener('input', toggleDisabled(name, email, message));
+    email.addEventListener('input', toggleDisabled(name, email, message));
+    message.addEventListener('input', toggleDisabled(name, email, message));
+  })();
+
+  function toggleDisabled(name, email, message) {
+    return function() {
+      var button = document.getElementById('submitButton');
+      if(name.value && email.value && message.value) {
+        button.removeAttribute('disabled','');
+      } else {
+        button.setAttribute('disabled', '');
+      }
+    };
+  }
+
+})()
+
+//Refactor to remove jQuery
 $(function() {
 
-    $("input,textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+  function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
+  $('#submitButton').on('click', function() {
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-});
+    event.preventDefault(); // prevent default submit behaviour
+    // get values from FORM
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val() ? $("#phone").val() : 'No phone number submitted';
+    var message = $("#message").val();
+    var nameErrorSpan = $('#name').prev('span');
+    var emailErrorSpan = $('#email').prev('span');
+    var messageErrorSpan = $('#message').prev('span');
 
+    name ? nameErrorSpan.text(''):nameErrorSpan.text('Please enter your name');
+    validateEmail(email) ? emailErrorSpan.text(''):emailErrorSpan.text('Please enter a valid email address');
+    message ? messageErrorSpan.text(''):messageErrorSpan.text('Please enter a message');
 
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-    $('#success').html('');
+    if (name && validateEmail(email) && message) {
+      var submitMessage = $('#success');
+      $.ajax({
+          url: "././mail/contact_me.php",
+          type: "POST",
+          data: {
+              name: name,
+              phone: phone,
+              email: email,
+              message: message
+          },
+          cache: false,
+          success: function() {
+              // Success message
+              submitMessage.text('Your message has been sent.');
+              submitMessage.addClass('submit-message');
+
+              //clear all fields
+              $('#contactForm').trigger("reset");
+          },
+          error: function() {
+              // Fail message
+              submitMessage.text('Sorry, it seems that my mail server is not responding. Please try again later.');
+              submitMessage.addClass('submit-message');
+              //clear all fields
+              $('#contactForm').trigger("reset");
+          },
+      })
+    } else {
+      return false;
+    }
+  });
+
+  /*When clicking on Full hide fail/success boxes */
+  $('#name, #email, #phone, #message').focus(function() {
+    var submitMessage = $('#success');
+      submitMessage.text('');
+      submitMessage.removeClass('submit-message');
+  });
+
 });
--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,61 @@
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+(function() {
+  openModal();
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+  function openModal() {
+    var portfolioLinks = document.querySelectorAll('.portfolio__item__link');
+    portfolioLinks.forEach(function(node) {
+      node.addEventListener('click', function() {
+       var body = document.getElementById('page-top');
+       var item = this.getAttribute('href').slice(1);
+       var modal = document.getElementById(item);
+       modal.style.display = 'block';
+       modal.setAttribute('aria-hidden', false);
+       body.className = 'body-modal-open';
+       closeModal(modal, body);
+      });
     });
-});
+  }
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+  function closeModal(modal, body) {
+    var closeButtons = document.querySelectorAll('[data-dismiss="modal"]');
+    closeButtons.forEach(function(node) {
+      node.addEventListener('click', function() {
+       modal.style.display = '';
+       modal.setAttribute('aria-hidden', true);
+       body.className = '';
+      });
+    });
+  }
+})()
+
+//Refactor to remove jQuery
+$(function() {
+  // jQuery to collapse the navbar on scroll
+  $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+          $(".navbar-fixed-top").addClass("top-nav-collapse");
+      } else {
+          $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      }
+  });
+
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+  });
+
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function() {
+      $('.navbar-toggle:visible').click();
+  });
+
+  $('.credits-link').click(function() {
+    $('footer').toggleClass('credits-link-expand');
+    $('.credits').slideToggle('slow');
+  });
+
 });
